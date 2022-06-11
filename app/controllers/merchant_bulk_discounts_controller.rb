4f1b05aa28ff9d045221289401068c7d442da6da
Caden Jarrett <caden.jarrett99@gmail.com>
2022-06-11 12:04:06 -0600

    Docs: added form to create a new merchant bulk discount

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -3,4 +3,19 @@ class MerchantBulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.bulk_discounts
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    BulkDiscount.create(bulk_discount_params)
+    redirect_to merchant_bulk_discounts_path(params[:merchant_id])
+  end
+
+  private
+
+  def bulk_discount_params
+    params.permit(:threshold, :percentage, :merchant_id)
+  end
 end
