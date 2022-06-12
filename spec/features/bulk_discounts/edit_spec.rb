785085f327ace45551244a5b3f480591c387fbf7
Caden Jarrett <caden.jarrett99@gmail.com>
2022-06-12 13:02:24 -0600

    Spec: added test file for editing a bulk discount

---
+++ spec/features/bulk_discounts/edit_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'discount show page', type: :feature do
+  before :each do
+    @merchant_1 = Merchant.create!(name: 'Brylan')
+    @merchant_2 = Merchant.create!(name: 'Ray')
+
+    @bulk_discount_1 = BulkDiscount.create!(percentage:15, threshold:10, merchant_id:@merchant_1.id)
+    @bulk_discount_2 = BulkDiscount.create!(percentage:10, threshold:12, merchant_id:@merchant_1.id)
+    @bulk_discount_3 = BulkDiscount.create!(percentage:25, threshold:16, merchant_id:@merchant_1.id)
+    @bulk_discount_4 = BulkDiscount.create!(percentage:20, threshold:8, merchant_id:@merchant_2.id)
+  end
+
+  it 'can edit a merchant bulk_discount' do
+    visit merchant_bulk_discount_path(@merchant_1, @bulk_discount_1)
+
+    click_on 'Edit Bulk Discount'
+
+    expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant_1, @bulk_discount_1))
+
+    within '#new-threshold' do
+      select(25)
+    end
+
+    within '#new-percentage' do
+      select(25)
+    end
+
+    click_on 'Create'
+
+    expect(current_path).to eq(merchant_bulk_discounts_path(@merchant_1))
+  end
+end
